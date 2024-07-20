a73ede372d43bce4fa828274f774086ee0c4ad15
Rashmi <5Rashmi>
2024-07-20 19:25:10 +0530

    added function to remove transition at 0 seconds

--- Clock/index.js
+++ Clock/index.js
@@ -4,17 +4,23 @@ const hourHand = document.querySelector(".hour-hand");
 
 function setDate(){
     const now = new Date();
-    
+
     const seconds = now.getSeconds();
     const secondsDegree = ((seconds/60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+
+    if (!seconds === 0){
+        secondHand.style.transition = 'all 0.09s linear';
+    } else {
+        secondHand.style.transition = 'none';
+    }
 
     const minutes = now.getMinutes();
     const minutesDegree = ((minutes/60) * 360) + 90;
     minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
 
     const hours = now.getHours();
-    const hoursDegree = ((hours/12) * 360) + 90;
+    const hoursDegree = (30 * hours + (minutes / 2)) + 90;
     hourHand.style.transform = `rotate(${hoursDegree}deg)`;
 }
 
